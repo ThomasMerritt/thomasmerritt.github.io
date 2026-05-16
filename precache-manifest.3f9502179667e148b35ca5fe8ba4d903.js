c65d47a1f6f9206e0439f85e90a73a8edbf9a6ea
ThomasMerritt <84432380+ThomasMerritt@users.noreply.github.com>
2026-05-16 09:24:08 -0700

    Updates

---
+++ precache-manifest.3f9502179667e148b35ca5fe8ba4d903.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61ba4cd984833a7f0b150b70d6a4d7b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1793b96b581b5aae8c8",
+    "url": "/static/css/main.21406fdf.chunk.css"
+  },
+  {
+    "revision": "b944baf259472084ebcc",
+    "url": "/static/js/2.cfa003c2.chunk.js"
+  },
+  {
+    "revision": "0742948f272a60bdf22d264c3e9be5d5",
+    "url": "/static/js/2.cfa003c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d8bad3b44a63bf33c7d",
+    "url": "/static/js/3.bc546824.chunk.js"
+  },
+  {
+    "revision": "a1793b96b581b5aae8c8",
+    "url": "/static/js/main.917df45f.chunk.js"
+  },
+  {
+    "revision": "7ad62a312e17e05f50e2",
+    "url": "/static/js/runtime-main.00aec48c.js"
+  },
+  {
+    "revision": "a7dfa7d21b252ecc8b5d1ec0d18d7130",
+    "url": "/static/media/Resume.a7dfa7d2.pdf"
+  },
+  {
+    "revision": "1c871112eb54086fbb1ebb7231ca07f0",
+    "url": "/static/media/cursed_image.1c871112.JPEG"
+  },
+  {
+    "revision": "6f793353deed7bde809cb43b91b95704",
+    "url": "/static/media/face.6f793353.jpeg"
+  },
+  {
+    "revision": "1e96bbd6e7c4164ecfe7823a006e3f9e",
+    "url": "/static/media/path.1e96bbd6.jpg"
+  },
+  {
+    "revision": "fa3a230f612fe1ef59eac578107a85d6",
+    "url": "/static/media/scape-no-human.fa3a230f.jpg"
+  },
+  {
+    "revision": "00ecdb5eac77758063baa4b240b47e6d",
+    "url": "/static/media/scape.00ecdb5e.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/personal-website-react/precache-manifest.c9b6570f0a6f6c286c2abb62edb5da3e.js"
+  "/precache-manifest.3f9502179667e148b35ca5fe8ba4d903.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/personal-website-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
